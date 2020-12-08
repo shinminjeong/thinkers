3677af75f91eb1020d27da42dbe9428e7dc951d5
Minjeong Shin <minjeong.shin@anu.edu.au>
2020-12-08 14:41:52 +0900

    make numPetal changeable

--- thnet/thnet/views.py
+++ thnet/thnet/views.py
@@ -29,11 +29,33 @@ def get_author_info(request):
 def flower(request):
     pageid = request.GET.get('id')
     flower_info = make_flower(pageid)
+
+    min_pub_year = max_pub_year = min_cite_year = max_cite_year = 1990
+    num_leaves = 25
+    config = {
+        "pub_lower": min_pub_year,
+        "pub_upper": max_pub_year,
+        "cit_lower": min_cite_year,
+        "cit_upper": max_cite_year,
+        "self_cite": "false",
+        "icoauthor": "true",
+        "cmp_ref": "false",
+        "num_leaves": num_leaves,
+        "order": "ratio",
+    }
     data = {
         "author": flower_info,
         "conf"  : [],
         "inst"  : [],
         "fos"   : [],
+        "yearSlider": {
+            "title": "Publications range",
+            "pubrange": [min_pub_year, max_pub_year, (max_pub_year-min_pub_year+1)],
+            "citerange": [min_cite_year, max_cite_year, (max_cite_year-min_cite_year+1)],
+            "pubChart": None,
+            "citeChart": None,
+            "selected": config
+        },
     }
 
     return render(request, "flower.html", data)
